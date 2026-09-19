44e78b3ad21cf6a7ea78fe53c065aefc9eb926e1
vitoradekz <vitorp@adekz.com>
2026-09-19 19:51:13 -0300

    correção do cenário de login com usuários obrigatório e inclusão da url base

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -1,52 +1,49 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect, Page } from "@playwright/test";
 
-test('usuário obrigatório', async ({ page }) => {
-  await login(page, '', 'senha123')
-  await toast(page, 'Informe o seu nome de usuário!s')
+test("usuário obrigatório", async ({ page }) => {
+  await login(page, "", "senha123");
+  await toast(page, "Informe o seu nome de usuário!");
 });
 
-test('senha obrigatória', async ({ page }) => {
-  await login(page,'qa', '')
-  await toast(page, 'Informe a sua senha secreta!')
-})
+test("senha obrigatória", async ({ page }) => {
+  await login(page, "qa", "");
+  await toast(page, "Informe a sua senha secreta!");
+});
 
-test('usuário não existe', async ({ page }) => {
-  await login(page,'teste', 'teste')
-  await toast(page, 'Oops! Credenciais inválidas :(')
-})
+test("usuário não existe", async ({ page }) => {
+  await login(page, "teste", "teste");
+  await toast(page, "Oops! Credenciais inválidas :(");
+});
 
-test('senha incorreta', async ({ page }) => {
-  await login(page,'qa', 'teste')
-  await toast(page, 'Oops! Credenciais inválidas :(')
-})
+test("senha incorreta", async ({ page }) => {
+  await login(page, "qa", "teste");
+  await toast(page, "Oops! Credenciais inválidas :(");
+});
 
-test('com sucesso', async ({ page }) => {
-  await login(page,'qa', 'xperience')
-  await modal(page, 'Suas credenciais são válidas :)')
-})
+test("com sucesso", async ({ page }) => {
+  await login(page, "qa", "xperience");
+  await modal(page, "Suas credenciais são válidas :)");
+});
 
 const toast = async (page: Page, message: string) => {
-  const target = page.locator('div[role=status]')
+  const target = page.locator("div[role=status]");
   await expect(target).toHaveText(message);
-}
+};
 
 const modal = async (page: Page, message: string) => {
-  const target = page.locator('.swal2-html-container')
+  const target = page.locator(".swal2-html-container");
   await expect(target).toHaveText(message);
-}
+};
 
 const login = async (page: Page, user: string, pass: string) => {
-    await page.goto('/')
+  await page.goto("/");
 
-    const username = page.locator('[name=user]')
-    const password = page.locator('[name=pass]')
+  const username = page.locator("[name=user]");
+  const password = page.locator("[name=pass]");
 
-    user 
-      ? await username.fill(user) : null
+  user ? await username.fill(user) : null;
 
-    pass
-      ? await password.fill(pass) : null
+  pass ? await password.fill(pass) : null;
 
-    await page.click('css=button >> text=Entrar')
-}
-
+  await page.click("css=button >> text=Entrar");
+};
